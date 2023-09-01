d5e05e8a4917d890c070c020e4def2522feb4a98
harshsinghcs <harshsinghcs01@gmail.com>
2023-09-02 00:25:07 +0530

    code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import random
 
 app = Flask(__name__)
@@ -8,31 +8,30 @@ class Plant:
         self.traits = traits
 
 def hybridize(parent1, parent2):
-    if len(parent1.traits) != len(parent2.traits):
-        raise ValueError("Parents must have the same number of traits")
-    
-    child_traits = []
-    for trait1, trait2 in zip(parent1.traits, parent2.traits):
-        child_trait = random.choice([trait1, trait2])
-        child_traits.append(child_trait)
-    
-    return Plant(child_traits)
+    # Same hybridize function as before
+    pass
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
-    if request.method == "POST":
-        parent1_traits = [request.form[f"parent1_{trait}"] for trait in trait_names]
-        parent2_traits = [request.form[f"parent2_{trait}"] for trait in trait_names]
-        
-        parent1 = Plant(parent1_traits)
-        parent2 = Plant(parent2_traits)
-        
-        child = hybridize(parent1, parent2)
-        
-        return render_template("index.html", parent1=parent1, parent2=parent2, child=child)
-    
     return render_template("index.html")
 
+@app.route("/hybridize", methods=["POST"])
+def hybridize_plants():
+    parent1_traits = [request.form["parent1_height"], request.form["parent1_color"]]  # Add more traits
+    parent2_traits = [request.form["parent2_height"], request.form["parent2_color"]]  # Add more traits
+
+    parent1 = Plant(parent1_traits)
+    parent2 = Plant(parent2_traits)
+
+    child = hybridize(parent1, parent2)
+
+    response = {
+        "parent1_traits": parent1.traits,
+        "parent2_traits": parent2.traits,
+        "child_traits": child.traits
+    }
+    
+    return jsonify(response)
+
 if __name__ == "__main__":
-    trait_names = ["Height", "Color", "Bloom Time"]
     app.run(debug=True)
