615902bb0e15aa420ca1238c5a41407656bd7f1c
Kom Senapati <Komnoob123@gmail.com>
2023-09-07 21:46:34 +0530

    Added the logic for hybridisation inside app.py

--- app.py
+++ app.py
@@ -3,35 +3,43 @@ import random
 
 app = Flask(__name__)
 
+
 class Plant:
     def __init__(self, traits):
         self.traits = traits
 
-def hybridize(parent1, parent2):
-    # Implement your hybridization logic here
-    pass
+    @staticmethod
+    def hybridize(plant1, plant2):
+        combined_traits = [random.choice(trait_pair) for trait_pair in
+                           zip(plant1.traits, plant2.traits)]
+        return Plant(combined_traits)
+
 
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
+
 @app.route("/hybridize", methods=["POST"])
 def hybridize_plants():
-    parent1_traits = [request.form["parent1_height"], request.form["parent1_color"]]  # Add more traits
-    parent2_traits = [request.form["parent2_height"], request.form["parent2_color"]]  # Add more traits
+    parent1_traits = [request.form["parent1_height"],
+                      request.form["parent1_color"]]  # Add more traits
+    parent2_traits = [request.form["parent2_height"],
+                      request.form["parent2_color"]]  # Add more traits
 
     parent1 = Plant(parent1_traits)
     parent2 = Plant(parent2_traits)
 
-    child = hybridize(parent1, parent2)
+    child = Plant.hybridize(parent1, parent2)
 
     response = {
         "parent1_traits": parent1.traits,
         "parent2_traits": parent2.traits,
         "child_traits": child.traits
     }
-    
+
     return jsonify(response)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
